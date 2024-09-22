a5321708bd5f3f28c86176280fde288e83e7c3cb
datawater <86855332+datawater@users.noreply.github.com>
2024-09-22 14:05:12 +0400

    NVM, this is the solution for p28

--- p28/src/main.rs
+++ p28/src/main.rs
@@ -1,7 +1,22 @@
-extern "C" {
-    fn run_main();
-}
-
 fn main() {
-    unsafe { run_main() };
+    let sq = 1001;
+    let mut d = 1;
+    let mut x = 0;
+
+    let term = |d: u32| -> u32 {
+        4 * d * d - 4 * d + 1
+    };
+
+    while 2 * d - 1 < sq {
+        x += term(d);
+        x += term(d) + 2 * d;
+        x += term(d) + 4 * d;
+        x += term(d) + 6 * d;
+
+        d += 1;
+    }
+
+    x += sq * sq;
+
+    println!("ANSWER P28: {x}");
 }
