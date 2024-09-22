38be686363e76d13579a21b99887b5b88c697550
datawater <86855332+datawater@users.noreply.github.com>
2024-09-22 18:16:48 +0400

    Remove check.py

--- p30/src/main.rs
+++ p30/src/main.rs
@@ -1,19 +1,21 @@
-use rayon::prelude::*;
-
 fn main() {
-    let x = (10..=1000000).into_par_iter().map(|x: u64| {
-        let mut sum = 0;
-        let mut y = x;
+    let x = (10..=1000000)
+        .map(|x: u64| {
+            let mut sum = 0;
+            let mut y = x;
 
-        while y != 0 {
-            sum += (y % 10).pow(5);
-            y /= 10;
-        }
+            while y != 0 {
+                sum += (y % 10).pow(5);
+                y /= 10;
+            }
 
-        if sum == x {
-            x 
-        } else {0}
-    }).sum::<u64>();
+            if sum == x {
+                x
+            } else {
+                0
+            }
+        })
+        .sum::<u64>();
 
     println!("SOLUTION P30: {x}");
 }
